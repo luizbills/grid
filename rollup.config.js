49f6afdd52234a0885d8c4036728e97c06916960
Luiz Bills <luizpbills@gmail.com>
2019-05-24 17:43:07 -0300

    add dist file banner

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,27 @@
 import { terser } from 'rollup-plugin-terser';
 import babel from 'rollup-plugin-babel';
+import banner from 'rollup-plugin-banner';
+
+const bannerContent = 'pkg.name v<%= pkg.version %> by <%= pkg.author %>\n<%= pkg.repository.url %> | pkg.license';
 
 const plugins = [
   babel({
     "presets": [
       "@babel/preset-env"
     ],
-  }),
+  })
 ];
 
 export default [{
   input: 'src/index.js',
   output: {
-    sourcemap: true,
+    sourcemap: false,
     format: 'iife',
     file: 'dist/grid.js'
   },
   plugins: [
     ...plugins,
+    banner(bannerContent)
   ],
   watch: {
     clearScreen: false
@@ -25,13 +29,14 @@ export default [{
 },{
   input: 'src/index.js',
   output: {
-    sourcemap: true,
+    sourcemap: false,
     format: 'iife',
     file: 'dist/grid.min.js'
   },
   plugins: [
     ...plugins,
-    terser()
+    terser(),
+    banner(bannerContent)
   ],
   watch: {
     clearScreen: false
